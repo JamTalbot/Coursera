23c900260ac29b86842f6f0394d96027f950d557
JamTalbot <69930426+JamTalbot@users.noreply.github.com>
2022-03-11 09:09:37 +0000

    Lect 57 update

--- Coursera-Assignments/Assignment-5/js/ajax-utils.js
+++ Coursera-Assignments/Assignment-5/js/ajax-utils.js
@@ -1,31 +1,89 @@
-// Returns an HTTP request object
-function getRequestObject() {
-    if (window.XMLHttpRequest) {
+// (function (global) {
+
+//     var ajaxUtils = {};
+    
+//     // Returns an HTTP request object
+//     function getRequestObject() {
+//         if (window.XMLHttpRequest) {
+//             return (new XMLHttpRequest());
+//         }
+//         else if (window.ActiveXObject) {
+//             // For very old IE Browsers
+//             return (new ActiveXObject("Microsoft.XMLHTTP"));
+//         }
+//         else {
+//             global.alert("Ajax is not supported.");
+//             return(null);
+//         }
+//     }
+    
+//     ajaxUtils.sendGetRequest = function(requestUrl, responseHandler) {
+//         var request = getRequestObject();
+//         request.onreadystatechange = function() {
+//             handleResponse(request, responseHandler);
+//         };
+//         request.open("GET", requestUrl, true);
+//         request.send(null);
+//     };
+    
+//     function handleResponse(request, responseHandler) {
+//         if ((request.readyState == 4) && (request.status == 200)) {
+//             responseHandler(request);
+//         }
+//     }
+    
+//     global.$ajaxUtils = ajaxUtils
+// })(window);
+
+(function (global) {
+
+    // Set up a namespace for our utility
+    var ajaxUtils = {};
+    
+    
+    // Returns an HTTP request object
+    function getRequestObject() {
+      if (global.XMLHttpRequest) {
         return (new XMLHttpRequest());
-    }
-    else if (window.ActiveXObject) {
-        // For very old IE Browsers
+      } 
+      else if (global.ActiveXObject) {
+        // For very old IE browsers (optional)
         return (new ActiveXObject("Microsoft.XMLHTTP"));
+      } 
+      else {
+        global.alert("Ajax is not supported!");
+        return(null); 
+      }
     }
-    else {
-        global.alert("Ajax is not supported.");
-        return(null);
-    }
-}
-
-ajaxUtils.sendGetRequest = function(requestUrl, responseHandler) {
-    var request = getRequestObject();
-    request.onreadystatechange = function() {
-        handleResponse(request, responseHandler);
-    };
-    request.open("GET", requestUrl, true);
-    request.send(null);
-};
-
-function handleResponse(request, responseHandler) {
-    if ((request.readyState == 4) && (request.status == 200)) {
+    
+    
+    // Makes an Ajax GET request to 'requestUrl'
+    ajaxUtils.sendGetRequest = 
+      function(requestUrl, responseHandler) {
+        var request = getRequestObject();
+        request.onreadystatechange = 
+          function() { 
+            handleResponse(request, responseHandler); 
+          };
+        request.open("GET", requestUrl, true);
+        request.send(null); // for POST only
+      };
+    
+    
+    // Only calls user provided 'responseHandler'
+    // function if response is ready
+    // and not an error
+    function handleResponse(request,
+                            responseHandler) {
+      if ((request.readyState == 4) &&
+         (request.status == 200)) {
         responseHandler(request);
+      }
     }
-}
-
-global.$ajaxUtils = ajaxUtils
+    
+    
+    // Expose utility to the global object
+    global.$ajaxUtils = ajaxUtils;
+    
+    
+    })(window);
--- Coursera-Assignments/Assignment-5/js/script.js
+++ Coursera-Assignments/Assignment-5/js/script.js
@@ -1,34 +1,18 @@
-// console.log(document.getElementById("title"));
-// document.addEventListener("DOMContentLoaded",
-//     function (event) {
-//         function sayHello () {
-//             console.log(this);
-//             console.log(event);
-//             this.textContent = "Said it!";
-//             var name = document.getElementById("name").value;
-//             var message = "<h2>Hello " + name + "!</h2>";
-//             document.getElementById("content").innerHTML = message;
-//             if (name == "student") {
-//                 var title = document.querySelector("#title").textContent;
-//                 title += " & Lovin' it!";
-//                 document.querySelector("#title").textContent = title;
-//             }
-//         }
-//         document.querySelector("button").onclick = sayHello;
-//     }
-// );
-
-// -----------------------
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    document.querySelector("button").addEventListener("click", function () {
-            var self = this;
-            var name = "";
-
-            $ajaxUtils.sendGetRequest("/data/name.txt", function (request) {
-                var name = request.ResponseText;
-                document.querySelector("#content").innerHTML = "<h2>Hello " + name + "!";
-            });
+document.addEventListener("DOMContentLoaded",
+function (event) {
+    
+    document.querySelector("button")
+    .addEventListener("click", function () {
+        
+        $ajaxUtils
+        .sendGetRequest("data/name.txt",
+        function (request) {
+            var name = request.responseText;
+            
+            document.querySelector("#content")
+            .innerHTML = "<h2>Hello " + name + "!";
+            console.log(name);
         });
-    }
+    });
+}
 );
